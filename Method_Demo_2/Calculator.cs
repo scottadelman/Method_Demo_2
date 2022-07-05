aeecfb5a1b54e0a0a393d5b88d04d78061598489
Scott <ScottAdelman97@gmail.com>
2022-07-05 10:07:03 -0600

    simple calculator

---
+++ Method_Demo_2/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demo_2
+{
+    internal class Calculator
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int c, int d)
+        {
+            return c / d;
+        }
+
+        public static int Difference(int e, int d)
+        {
+            return e - d;
+        }
+    }
+}
---
+++ Method_Demo_2/Program.cs
@@ -0,0 +1,6 @@
+﻿using Method_Demo_2;
+
+Console.WriteLine(Calculator.Sum(2, 5));
+Console.WriteLine(Calculator.Multiply(9, 8));
+Console.WriteLine(Calculator.Divide(4, 2));
+Console.WriteLine(Calculator.Difference(99, 50));
